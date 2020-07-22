0cec3d71956a7585a173a46e36d7a1b86554336c
zzr716 <zzr716@163.com>
2020-07-22 23:20:32 +0800

    sc

--- src/views/Info/index.vue
+++ src/views/Info/index.vue
@@ -23,6 +23,15 @@
       <el-table-column prop="createDate" label="日期" width="180"></el-table-column>
       <el-table-column prop="content" label="姓名" width="180"></el-table-column>
       <el-table-column prop="title" label="地址"></el-table-column>
+      <el-table-column
+      fixed="right"
+      label="操作"
+      width="100">
+      <template slot-scope="scope">
+        <el-button type="text" size="small">查看</el-button>
+        <el-button type="text" size="small">编辑</el-button>
+      </template>
+    </el-table-column>
     </el-table>
     <div class="block">
     <span class="demonstration">完整功能</span>
