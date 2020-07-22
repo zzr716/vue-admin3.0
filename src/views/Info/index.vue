ee68c34068861e38af401f452c5d161f4d352646
zzr716 <zzr716@163.com>
2020-07-22 23:26:52 +0800

    sc

--- src/views/Info/index.vue
+++ src/views/Info/index.vue
@@ -26,7 +26,7 @@
       <el-table-column
       fixed="right"
       label="操作"
-      width="100">
+      width="160">
       <template slot-scope="scope">
         <el-button type="text" size="small">查看</el-button>
         <el-button type="text" size="small">编辑</el-button>
